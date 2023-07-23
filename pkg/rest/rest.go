7f45b6733137a7ab24efb143d987b05d5dfa8614
kam1dere <kam1dere@yandex.ru>
2023-07-23 17:52:11 +0300

    feat: add rest

--- pkg/rest/rest.go
+++ pkg/rest/rest.go
@@ -1,7 +1,14 @@
 package rest
 
 import (
+	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/kam1dere/CSVDataService/config"
+	"github.com/kam1dere/CSVDataService/grpc/genproto/csvDataService"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"net/http"
 )
 
 type Rest struct {
@@ -15,17 +22,17 @@ func NewRest(cfg *config.Config) *Rest {
 }
 
 func (r *Rest) NewRestServer() {
-	//ctx := context.Background()
-	//ctx, cancel := context.WithCancel(ctx)
-	//defer cancel()
-	//mux := runtime.NewServeMux()
-	//opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	//err := csvDataService.RegisterServiceHandlerFromEndpoint(ctx, mux, r.cfg.Server.Address, opts)
-	//if err != nil {
-	//	log.Fatal().Err(err)
-	//}
-	//log.Info().Msgf("server listening at %s", r.cfg.Server.RestServer)
-	//if err := http.ListenAndServe(r.cfg.Server.RestServer, mux); err != nil {
-	//	log.Fatal().Err(err)
-	//}
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := csvDataService.RegisterCsvDataServiceHandlerFromEndpoint(ctx, mux, r.cfg.Server.Address, opts)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	log.Info().Msgf("server listening at %s", r.cfg.Server.RestServer)
+	if err := http.ListenAndServe(r.cfg.Server.RestServer, mux); err != nil {
+		log.Fatal().Err(err)
+	}
 }
